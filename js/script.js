c378ec17d7f458374d761f01250dc6414e62781a
motywacja <wm.motywacja@gmail.com>
2020-08-15 12:47:36 +0200

    add contact page + fix sections widths + scroll to archived news

--- js/script.js
+++ js/script.js
@@ -9,4 +9,14 @@ $(function() {
   $('div.img a.com').css({"border-bottom-left-radius":"6px", "border-bottom-right-radius":"6px", "-moz-border-radius-bottomleft":"6px", "-moz-border-radius-bottomright":"6px", "-webkit-border-bottom-left-radius":"6px", "-webkit-border-bottom-right-radius":"6px"});
   $('div.img img.fl').css({"border-top-left-radius":"6px", "border-top-right-radius":"6px", "-moz-border-radius-topleft":"6px", "-moz-border-radius-topright":"6px", "-webkit-border-top-left-radius":"6px", "-webkit-border-top-right-radius":"6px"});
 
-});	
+  $('.sidebar').find('.newsLink').each((i, e)=>{
+    let link = $(e);
+    let target_id = link.attr('target');
+    link.click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $(`#${target_id}`).offset().top - 30
+      }, 800);
+    });
+  });
+
+});
--- motywacja.py
+++ motywacja.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 import sys
 
@@ -7,13 +8,32 @@ from staticjinja import Site
 
 if __name__ == "__main__":
     news_file_path = os.path.join(os.path.dirname(__file__), 'contents/news.json')
-    news_context = json.loads(open(news_file_path).read())
-    news_context = sorted(news_context, key=lambda x: x.get('date'))
-    context = {
-        'news': news_context
+    contacts_file_path = os.path.join(os.path.dirname(__file__), 'contents/contacts.json')
+    about_file_path = os.path.join(os.path.dirname(__file__), 'contents/about.json')
+    footer_file_path = os.path.join(os.path.dirname(__file__), 'contents/footer.json')
+
+    footer_context = json.loads(open(footer_file_path).read())
+
+    news = json.loads(open(news_file_path).read())
+    news = sorted(news, key=lambda x: x.get('date'))
+    pages = math.ceil(len(news) / 20)
+    news = news[:20]
+    main_context = {
+        'news': news,
+        'pages': pages
     }
 
+    contact_context = json.loads(open(contacts_file_path).read())
+
+    about_context = json.loads(open(about_file_path).read())
+
+    all_views_contexts = [main_context, contact_context, about_context]
+    for context in all_views_contexts:
+        context['footer'] = footer_context
+
     site = Site.make_site(contexts=[
-        ('index.html', context),
+        ('index.html', main_context),
+        ('about.html', about_context),
+        ('contact.html', contact_context)
     ])
     site.render(use_reloader=int(sys.argv[1]) == 1 if len(sys.argv) > 1 else False)
